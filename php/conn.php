a6c524a0176011620f2c5260b26f7119857d1fd9
abdisalam02 <niwache12@gmail.com>
2024-11-27 17:25:03 +0100

    index.php and collections page

--- php/conn.php
+++ php/conn.php
@@ -11,8 +11,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Database connected successfully!";
-}
+} 
 ?>
 
